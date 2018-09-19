9b4562af4d8312b93ddc84f9ff3746acf5f71f59
Ebude <ebude.yolande@aims-cameroon.org>
2018-09-19 16:41:20 +0100

    Add files via upload

---
+++ _4DIGITS.ino
@@ -0,0 +1,192 @@
+
+/*
+  Showing number 0-9 on a Common Anode 7-segment LED display
+  Displays the numbers 0-9 on the display, with one second inbetween.
+    E
+   ---
+D |   | F
+  | G |
+   ---
+C |   | A
+  |   |
+   ---
+    B
+  Done by Ebude Yolande
+ */
+ 
+// Pin 2-8 is connected to the 7 segments of the display.
+
+int pinA = 9;
+int pinB = 10;
+int pinC = 11;
+int pinD = 12;
+int pinE = 6;
+int pinF = 7;
+int pinG = 8;
+int D1 = 2;
+int D2 = 3;
+int D3 = 4;
+int D4 = 5;
+
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pins as outputs.
+  Serial.begin(9600);
+  pinMode(pinA, OUTPUT);     
+  pinMode(pinB, OUTPUT);     
+  pinMode(pinC, OUTPUT);     
+  pinMode(pinD, OUTPUT);     
+  pinMode(pinE, OUTPUT);     
+  pinMode(pinF, OUTPUT);     
+  pinMode(pinG, OUTPUT);   
+  pinMode(D1, OUTPUT);  
+  pinMode(D2, OUTPUT);  
+  pinMode(D3, OUTPUT);  
+  pinMode(D4, OUTPUT);  
+ 
+  
+  
+}
+
+// Function that displays a number on a position
+void DigitPos(int x,int y){ 
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW);
+  
+  if (y==4){digitalWrite(D1, HIGH);}
+  if (y==3){digitalWrite(D2, HIGH);}
+  if (y==2){digitalWrite(D3, HIGH);}
+  if (y==1){digitalWrite(D4, HIGH);}
+  
+  if (x==0){
+  //0
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);   
+  delay(5);}
+ 
+  if (x==1){
+       //1
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);   
+  delay(5);}
+  
+  if (x==2){
+   //2
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);   
+  delay(5);}
+  
+  if (x==3){
+       //3
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);   
+  delay(5);}
+  
+  if (x==4){
+     //4
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);   
+  delay(5);}
+  
+ if (x==5){
+    //5
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, LOW);   
+  delay(5);}
+  
+ if (x==6){
+   //6
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, LOW);   
+  delay(5);}
+
+if (x==7){
+ //7
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);   
+  delay(5);}  
+  
+ if (x==8){
+   //8
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);   
+  delay(5);}
+
+ if (x==9){
+   //9
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);   
+  delay(5);}
+ 
+}
+
+// Function displays numbers
+void DisplayNum(int x){
+ int i=1;
+ int a=0;
+ while (x>10) {
+      a=x%10;
+      x=x/10;
+      DigitPos(a,i);
+      i+=1;   
+ }
+ DigitPos(x,i);
+
+}
+
+void loop(){
+  DisplayNum(156);
+ }
